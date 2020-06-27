b0477dc7aa355b56f72318260f81497bff744276
omanof <rachmanj@gmail.com>
2020-06-27 12:01:03 +0800

    profile default image

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,12 @@ class Profile extends Model
 {
     protected $guarded = [];
 
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/WJC6ZUsFFB96fqRjXcODXibAnD48VKr87qN3N0TY.jpeg';
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
